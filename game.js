0aefef999be6afd32e0ce3319687eb16041f8a98
Thomas Cailhol <thomas@plume-app.co>
2023-12-05 11:21:37 +0000

    fix query fetching

--- game.js
+++ game.js
@@ -1,66 +1,66 @@
 let sp;
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  sp = 
+  sp =
     supabase.createClient('https://fyggsmdxumjqcmqkdizz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5Z2dzbWR4dW1qcWNtcWtkaXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEzMzU3NTYsImV4cCI6MjAxNjkxMTc1Nn0.c_-UlBxlaFZZcj9FKr8u33r2TE7KZSt_-MzijCPlB94');
   console.log(sp);
   const startGameButton = document.getElementById('start-game');
   const rulesButton = document.getElementById('rules-button');
   // add event listenier to start game button
   startGameButton.addEventListener('click', function () {
-      console.log('display queries');
-      toggleMenu();
-      togglePlayerInput();
+    console.log('display queries');
+    toggleMenu();
+    togglePlayerInput();
   });
   // add event listener to rules button
   rulesButton.addEventListener('click', function () {
-      console.log('display rules');
-      toggleRules();
+    console.log('display rules');
+    toggleRules();
   });
   const form = document.getElementById('new-player-form'); // Replace with your form's ID
-  form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      submitPlayerName();
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    submitPlayerName();
   });
 });
 
 class GameSession {
   constructor(query, queryIndex, playerName) {
-      this.query = query;
-      this.score = 0;
-      this.position = 15;
-      this.usedCards = [];
-      this.gameOver = false;
-      this.playerName = playerName;
-      this.queryIndex = queryIndex;
+    this.query = query;
+    this.score = 0;
+    this.position = 15;
+    this.usedCards = [];
+    this.gameOver = false;
+    this.playerName = playerName;
+    this.queryIndex = queryIndex;
   }
 
   chooseCard(index) {
-      const card = this.query.cards[index];
-      this.usedCards.push(card);
+    const card = this.query.cards[index];
+    this.usedCards.push(card);
 
-      this.position = Math.max(1, Math.min(15, this.position - card.impact)); // minus because the goal is to go first
-      
-      if (this.position === 1) {
-          this.endGame('won');
-      } else if (this.position === 15 || this.usedCards.length >= 6) {
-          this.endGame('lost');
-      }
+    this.position = Math.max(1, Math.min(15, this.position - card.impact)); // minus because the goal is to go first
+
+    if (this.position === 1) {
+      this.endGame('won');
+    } else if (this.position === 15 || this.usedCards.length >= 6) {
+      this.endGame('lost');
+    }
   }
 
   endGame(outcome) {
-      this.gameOver = true;
-      disableAllCards();
-      this.computeFinalScore();
+    this.gameOver = true;
+    disableAllCards();
+    this.computeFinalScore();
 
-      console.log('Your score is ' + this.score)
-      if (outcome === 'won') {
-          console.log('You won!');
-      } else {
-          console.log('You lost!');
-      }
+    console.log('Your score is ' + this.score)
+    if (outcome === 'won') {
+      console.log('You won!');
+    } else {
+      console.log('You lost!');
+    }
 
-      submitGameSessionToSupabase(this.playerName, this.position, this.usedCards.length, this.score, this.query.name, this.score);
+    submitGameSessionToSupabase(this.playerName, this.position, this.usedCards.length, this.score, this.query.name, this.score);
   }
 
 
@@ -75,9 +75,31 @@ class GameSession {
   }
 }
 
-let { data: Queries, error } = await sp
-  .from('Query')
-  .select('*')
+// Call the function
+
+async function getQueries() {
+  const queryTable = sp.from('Query');
+
+  try {
+    const result = await queryTable.select('*');
+    console.log('Queries retrieved from Supabase:', result);
+    return result.data; // Return the data
+  } catch (error) {
+    console.error('Error retrieving queries from Supabase:', error);
+    throw error; // Re-throw the error to be handled elsewhere if needed
+  }
+}
+
+let queries;
+
+getQueries()
+  .then(data => {
+    console.log('Queries:', data);
+    queries = data;
+  })
+  .catch(error => {
+    // Handle errors here if needed
+  });
 
 let currentSession;
 
@@ -88,14 +110,14 @@ function displayQueries(playerName) {
   queriesContainer.innerHTML = '';
 
   queries.forEach((query, index) => {
-      const queryButton = document.createElement('a');
-      queryButton.className = 'query';
-      queryButton.textContent = query.name;
-      queryButton.addEventListener('click', function() {
-          startNewSession(index, playerName);
-      });
+    const queryButton = document.createElement('a');
+    queryButton.className = 'query';
+    queryButton.textContent = query.name;
+    queryButton.addEventListener('click', function () {
+      startNewSession(index, playerName);
+    });
 
-      queriesContainer.appendChild(queryButton);
+    queriesContainer.appendChild(queryButton);
   });
 }
 
@@ -151,7 +173,7 @@ function onCardChoice(index, cardElement) {
   currentSession.chooseCard(index);
 
   cardElement.classList.add('used');
-  
+
   const impactWrapper = cardElement.querySelector('.impact-wrapper');
   // add a paragraph inside impact wrapper sith class impact-score
   const impactScore = document.createElement('p');
@@ -176,15 +198,15 @@ function hideQueries() {
 }
 
 function updateGameState(index) {
-  console.log("Impact: " + card.impact  + " Score: " + currentSession.score)
+  console.log("Impact: " + card.impact + " Score: " + currentSession.score)
   // currentSession.score = currentSession.position - queries;
 }
 
 function checkForEndGame() {
   if (currentSession.position <= 1) {
-      currentSession.endGame('won');
+    currentSession.endGame('won');
   } else if (currentSession.usedCards.length >= 6) {
-      currentSession.endGame('lost');
+    currentSession.endGame('lost');
   }
 }
 
@@ -205,11 +227,11 @@ function updateRankingDisplay() {
 
   const results = [...queries[0].ranking]; // Clone the ranking array
   const userSite = {
-      id: 15, // Assign a unique ID for the user's site
-      title: 'Your Site', // Custom title for the user's site
-      url: 'https://www.yoursite.com', // Custom URL for the user's site
-      description: 'Your site description.', // Custom description
-      userSite: true // Flag to identify the user's site
+    id: 15, // Assign a unique ID for the user's site
+    title: 'Your Site', // Custom title for the user's site
+    url: 'https://www.yoursite.com', // Custom URL for the user's site
+    description: 'Your site description.', // Custom description
+    userSite: true // Flag to identify the user's site
   };
 
   // Assuming that the maximum number of moves to win is 6 (as per your game logic)
@@ -228,37 +250,37 @@ function updateRankingDisplay() {
   results.splice(userSitePosition - 1, 0, userSite);
 
   results.forEach((result, index) => {
-      const resultDiv = document.createElement('div');
-      resultDiv.className = 'google-result';
+    const resultDiv = document.createElement('div');
+    resultDiv.className = 'google-result';
 
-      // Create elements for title, URL, and description
-      const title = document.createElement('div');
-      title.className = 'title';
-      title.textContent = (index + 1) + '. ' + result.title;
+    // Create elements for title, URL, and description
+    const title = document.createElement('div');
+    title.className = 'title';
+    title.textContent = (index + 1) + '. ' + result.title;
 
-      const url = document.createElement('div');
-      url.className = 'url';
-      url.textContent = result.url;
+    const url = document.createElement('div');
+    url.className = 'url';
+    url.textContent = result.url;
 
-      const description = document.createElement('div');
-      description.className = 'description';
-      description.textContent = result.description;
+    const description = document.createElement('div');
+    description.className = 'description';
+    description.textContent = result.description;
 
-      // Append elements to the resultDiv
-      resultDiv.appendChild(title);
-      resultDiv.appendChild(url);
-      resultDiv.appendChild(description);
+    // Append elements to the resultDiv
+    resultDiv.appendChild(title);
+    resultDiv.appendChild(url);
+    resultDiv.appendChild(description);
 
-      // Highlight the user's site
-      if (result.userSite) {
-          resultDiv.classList.add('user-position'); // Add a class for custom styling
-          // add n empty div inside the resultDiv taht would be positioned absolutely 80px up from the top
-          const userSiteScrollPosition = document.createElement('div');
-          userSiteScrollPosition.classList.add('user-site-scroll-position');
-          resultDiv.appendChild(userSiteScrollPosition);
-      }
+    // Highlight the user's site
+    if (result.userSite) {
+      resultDiv.classList.add('user-position'); // Add a class for custom styling
+      // add n empty div inside the resultDiv taht would be positioned absolutely 80px up from the top
+      const userSiteScrollPosition = document.createElement('div');
+      userSiteScrollPosition.classList.add('user-site-scroll-position');
+      resultDiv.appendChild(userSiteScrollPosition);
+    }
 
-      googleResultsDiv.appendChild(resultDiv);
+    googleResultsDiv.appendChild(resultDiv);
   });
 
   // add overflow-y scroll to game-grid-column
@@ -269,14 +291,14 @@ function updateRankingDisplay() {
   const userSiteElement = document.querySelector('.user-site-scroll-position');
   // set timeout to wait for the results to be rendered
   setTimeout(() => {
-      userSiteElement.scrollIntoView({ behavior: 'smooth' });
+    userSiteElement.scrollIntoView({ behavior: 'smooth' });
   }, 3);
 }
 
 function generateFakeGoogleResults(count) {
   const fakeResults = [];
   for (let i = 0; i < count; i++) {
-      fakeResults.push('Result ' + (i + 1));
+    fakeResults.push('Result ' + (i + 1));
   }
   return fakeResults;
 }
@@ -287,51 +309,51 @@ function displayCards(cards) {
   container.innerHTML = '';
 
   cards.forEach((card, cardIndex) => {
-      const gameCard = document.createElement('div');
-      gameCard.classList.add('game-card');
+    const gameCard = document.createElement('div');
+    gameCard.classList.add('game-card');
 
-      const gameCardInner = document.createElement('div');
-      gameCardInner.classList.add('game-card-inner');
+    const gameCardInner = document.createElement('div');
+    gameCardInner.classList.add('game-card-inner');
 
-      const gameCardTitleWrapper = document.createElement('div');
-      gameCardTitleWrapper.classList.add('game-card-title-wrapper');
+    const gameCardTitleWrapper = document.createElement('div');
+    gameCardTitleWrapper.classList.add('game-card-title-wrapper');
 
-      const gameCardDescriptionWrapper = document.createElement('div');
-      gameCardDescriptionWrapper.classList.add('game-card-description-wrapper');
+    const gameCardDescriptionWrapper = document.createElement('div');
+    gameCardDescriptionWrapper.classList.add('game-card-description-wrapper');
 
-      const impactWrapper = document.createElement('div');
-      impactWrapper.classList.add('impact-wrapper');
-      
-      gameCard.appendChild(gameCardInner);
-      gameCard.setAttribute('data-index', cardIndex);
+    const impactWrapper = document.createElement('div');
+    impactWrapper.classList.add('impact-wrapper');
 
-      const title = `
+    gameCard.appendChild(gameCardInner);
+    gameCard.setAttribute('data-index', cardIndex);
+
+    const title = `
       <p class="game-card-title">${card.name}</h3>
       `;
 
-      gameCardTitleWrapper.innerHTML = title;
-      
-      const description = `
+    gameCardTitleWrapper.innerHTML = title;
+
+    const description = `
       <p class="game-card-description">${card.description}</p>
       `;
 
-      gameCardDescriptionWrapper.innerHTML = description;
+    gameCardDescriptionWrapper.innerHTML = description;
 
-      gameCardInner.innerHTML = ``  
-      gameCardInner.appendChild(gameCardTitleWrapper); // Add gameCardTitleWrapper to gameCardInner
-      gameCardInner.appendChild(gameCardDescriptionWrapper); // Add gameCardDescriptionWrapper to gameCardInner
-      gameCardInner.appendChild(impactWrapper); // Add impactWrapper to gameCardInner
+    gameCardInner.innerHTML = ``
+    gameCardInner.appendChild(gameCardTitleWrapper); // Add gameCardTitleWrapper to gameCardInner
+    gameCardInner.appendChild(gameCardDescriptionWrapper); // Add gameCardDescriptionWrapper to gameCardInner
+    gameCardInner.appendChild(impactWrapper); // Add impactWrapper to gameCardInner
 
-      gameCard.addEventListener('click', cardClickHandler);
+    gameCard.addEventListener('click', cardClickHandler);
 
-      container.appendChild(gameCard);
+    container.appendChild(gameCard);
   });
 }
 
 function cardClickHandler(event) {
   if (currentSession.gameOver) {
-      console.log('The game is over. Please start a new game.');
-      return;
+    console.log('The game is over. Please start a new game.');
+    return;
   }
 
   const cardElement = event.currentTarget;
@@ -343,8 +365,8 @@ function cardClickHandler(event) {
 function disableAllCards() {
   const cards = document.getElementsByClassName('game-card');
   for (let card of cards) {
-      card.classList.add('used');
-      card.removeEventListener('click', cardClickHandler);
+    card.classList.add('used');
+    card.removeEventListener('click', cardClickHandler);
   }
 }
 
@@ -357,10 +379,10 @@ function submitGameSessionToSupabase(playerName, finalPosition, cardsUsed, score
   const gameSessionsTable = sp.from('Game');
 
   gameSessionsTable.insert([
-      { player_name: playerName, final_position: finalPosition, cards_used: cardsUsed, computed_score: score, query: query, total_impact: total_impact }
+    { player_name: playerName, final_position: finalPosition, cards_used: cardsUsed, computed_score: score, query: query, total_impact: total_impact }
   ]).then(result => {
-      console.log('Game session submitted to Supabase:', result);
+    console.log('Game session submitted to Supabase:', result);
   }).catch(error => {
-      console.error('Error submitting game session to Supabase:', error);
+    console.error('Error submitting game session to Supabase:', error);
   });
 }
